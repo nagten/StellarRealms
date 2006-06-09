1bb6a4c331361d383a8aec417a027f552c41a237
nagten <68544113+nagten@users.noreply.github.com>
2006-06-09 10:14:13 +0000

    SR commerce script

---
+++ DEV/GMScripts/SR/srcommerce.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          SR Commerce
+// @namespace     SRCommerce
+// @description   Calculate average commerce price
+// @include       http://sr.primeaxiom.com/commerce.asp
+// ==/UserScript==
+
+var quantity;
+var price;
+var table = document.body.childNodes[7].childNodes[3].childNodes[5].childNodes[1].childNodes[1];
+var tablelength = table.childNodes.length;
+
+//Skip the 3 first lines (table header) and advance with 2 (tableborder)
+for (var intI=4; intI<tablelength; intI = intI + 2)
+{
+	//get the quantity out the table
+ 	quantity = table.childNodes[intI].childNodes[3].firstChild.nodeValue;
+ 	
+ 	//get the price out the table
+ 	price = table.childNodes[intI].childNodes[7].firstChild.nodeValue;
+ 
+ 	//Calculate the average price and append the Price table element with the average price 
+ 	table.childNodes[intI].childNodes[7].firstChild.nodeValue = price + ' (' + price/quantity +')';	
+}
