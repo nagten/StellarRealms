0f0f4861f962374a34c2b87763ddfa012fe121c5
nagten <68544113+nagten@users.noreply.github.com>
2006-10-21 13:51:06 +0000

    Added a folder remotely

---
+++ DEV/PHP/Cabal/cabal_database.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_SERVER['SERVER_NAME'] == 'www.idsfadt.com') 
+{
+	$dbconn = mysql_connect("MySQL.alignus.com", "username", "password");
+	$result = mysql_select_db('xalignus-1',$dbconn) or die("DB Open Failure: " . mysql_error());
+}
+else 
+{
+	$dbconn = mysql_connect("localhost", "root", "root");
+	$result = mysql_select_db('sr') or die("DB Open Failure: " . mysql_error());
+}
+?>
