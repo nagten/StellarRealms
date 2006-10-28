bf71c31dacc5e0ace3ca1d5b5e7174beca2a1ffd
nagten <68544113+nagten@users.noreply.github.com>
2006-10-28 09:27:56 +0000

    Implemented rank code fixed updating and fixed a squid error "GET http"

--- DEV/PHP/Cabal/Scout/rank.php
+++ DEV/PHP/Cabal/Scout/rank.php
@@ -10,16 +10,16 @@ function UpdateRank($dbTurn)
 	$crlf = chr(13) . chr(10);
 	$tab  = chr(9);
 	$lf = "\n";
-	
+
 	$SQL = 'SELECT * FROM tblplanet ORDER BY Rank';
 	$result = mysql_query($SQL);
-	
-	if (!$result) 
+
+	if (!$result)
 	{
 		die('Invalid query: ' . mysql_error());
 	}
-		
-	while ($row = mysql_fetch_assoc($result)) 
+
+	while ($row = mysql_fetch_assoc($result))
 	{
 		$pID      = $row['RecordNumber'];
 		$pName    = stripslashes($row['PlanetName']);
@@ -35,31 +35,31 @@ function UpdateRank($dbTurn)
 		$pIndex[$indx] = $pName;
 	}
 	mysql_free_result($result);
-		
+
 	//--- GET XML FROM STELLAR-REALMS ---------------------------------------------------------------------------
 	$host = 'www.sr.primeaxiom.com';
 	$doc  = 'http://sr.primeaxiom.com/webservices/galaxy_overview.asp?un=' . urlencode('apiusers') . '&pwd=hitcvjnjrc';
-	$cmd  = 'GET ' . $doc . $crlf . '  HTTP/1.0' . $crlf . 'Host: ' . $host  . $crlf . $crlf;
-	
+	$cmd  = 'GET ' . $doc . '  HTTP/1.0' . $crlf . 'Host: ' . $host  . $crlf . $crlf;
+
 	$attempt = 0;
-		
-	do 
+
+	do
 	{
 		$attempt++;
 		echo date('M d Y  H:i:s') . ' Attempt ' . $attempt . $lf;
-		$fp = @fsockopen ('www.primeaxiom.com', 80, $errno, $errstr, 10); 
-		if (!$fp) 
-		{ 
+		$fp = @fsockopen ('www.primeaxiom.com', 80, $errno, $errstr, 10);
+		if (!$fp)
+		{
 			echo "Error opening socket: $errstr ($errno)\n";
 			if ($attempt > 10) die('Failed to connect to www.sr.primeaxion.com');
 		}
-	} 
+	}
 	while (! $fp);
-		
+
 	$resp = '';
 	fputs ($fp, $cmd);
-	
-	while (!feof($fp)) 
+
+	while (!feof($fp))
 	{
 		$resp .= fgets ($fp,128);
 	}
@@ -67,7 +67,7 @@ function UpdateRank($dbTurn)
 
 	$pos = strpos($resp,'<?xml version=');
 	$xmlstr = substr($resp,$pos);
-	
+
 	//--- PARSE THE XML --------------------------------------------------------------------------------------------
 	$xml = new XML('string',$xmlstr);
 
@@ -75,21 +75,22 @@ function UpdateRank($dbTurn)
 	$expression = '//go:context';
 	$context    = '';
 	$results = $xml->evaluate($expression, $context);
-	foreach ( $results as $result ) 
+	foreach ( $results as $result )
 	{
 		$a = array();
 		$a = $xml->get_attributes($result);
 		$srTurn = $a['turn'];
 	}
-		
+
 	//--- Process all the planets ---
-	if ($dbTurn != $srTurn || $srTurn == 0) 
+	if ($dbTurn != $srTurn || $srTurn == 0)
 	{
 		$expression = '//go:planet';
 		$context    = '';
 		$results = $xml->evaluate($expression, $context);
-		
-		foreach ( $results as $result ) 
+		$current_date = date("m-d-Y H:i:s");
+
+		foreach ( $results as $result )
 		{
 			$a = array();
 			$a = $xml->get_attributes($result);
@@ -108,18 +109,19 @@ function UpdateRank($dbTurn)
 			$pLeader  = addslashes($leader);
 			$pSpecies = addslashes($species);
 
-			echo $rank . $tab . $pName; //. $tab . $pLeader . $tab . $pSpecies . $tab . $online . $tab . $prestige . $lf;
+			//echo $rank . $tab . $pName; //. $tab . $pLeader . $tab . $pSpecies . $tab . $online . $tab . $prestige . $lf;
 
 			//--- New Planet? - Add to Database ---
-			if ( ! array_key_exists($name,$planets)) 
+			if ( ! array_key_exists($name,$planets))
 			{
-				$SQL  = 'INSERT INTO tblplanet (PlanetName,Leader,Species,Rank,FirstTurn,FirstRank) VALUES (';
+				$SQL  = 'INSERT INTO tblplanet (PlanetName,Leader,Species,Rank,FirstTurn,FirstRank, date) VALUES (';
 				$SQL .= '\'' . $pName    . '\',';
 				$SQL .= '\'' . $pLeader  . '\',';
 				$SQL .= '\'' . $pSpecies . '\',';
 				$SQL .= '\'' . $rank     . '\',';
 				$SQL .= '\'' . $srTurn   . '\',';
-				$SQL .= '\'' . $rank     . '\')';
+				$SQL .= '\'' . $rank   . '\',';
+				$SQL .= '\'' . $current_date . '\')';
 				//echo $SQL . $lf;
 				$result = mysql_query($SQL);
 				$pIndex[] = $pName;
@@ -127,19 +129,19 @@ function UpdateRank($dbTurn)
 				$SQL = 'SELECT RecordNumber FROM tblplanet WHERE PlanetName = \'' . $pName . '\'';
 				$result = mysql_query($SQL);
 				if (!$result)
-				{ 
+				{
 					die('Invalid query: ' . mysql_error());
 				}
-				
+
 				if ($row = mysql_fetch_assoc($result))
 				{
 					$pID = $row['RecordNumber'];
-				} 
-				else 
+				}
+				else
 				{
 					die('Selected nothing');
 				}
-				
+
 				//$pID = mysql_insert_id($dbconn);
 				$planets[$name]['ID']      = $pID;
 				$planets[$name]['Name']    = $pName;
@@ -149,22 +151,23 @@ function UpdateRank($dbTurn)
 			}
 
 			//--- Update the Planet Table ---
-			$SQL = 'UPDATE tblplanet SET ';
-			$SQL .= 'Rank = ' . $rank . ', ';
-			
-			$SQL .= 'TurnCount= ' . $srTurn  . ' ';
-			$SQL .= 'WHERE PlanetName = \'' . $pName . '\'';
-			
-			echo $SQL . $lf;
-			
+			$SQL = "UPDATE tblplanet SET Rank = " . $rank . ", TurnCount= " . $srTurn  . ", date= '" . $current_date  . "' ";
+			$SQL .= " WHERE PlanetName = '" . $pName . "' ";
+
+			//echo $SQL . $lf;
+
 			$result = mysql_query($SQL);
-			if (!$result) 
-			{	
+			if (!$result)
+			{
 				die('Invalid query: ' . mysql_error());
+			}
+			else
+			{
+				//echo "updated";
 			}
 		}
 	}
 	unset($xml);
 	unset($xmlstr);
-}    
+}
 ?>
