40b8951e8b289489455a1f808760a9fc03c1a7bf
nagten <68544113+nagten@users.noreply.github.com>
2006-11-01 11:51:29 +0000

    Added Murc and Shriners

--- DEV/PHP/Cabal/variables.php
+++ DEV/PHP/Cabal/variables.php
@@ -1,25 +1,51 @@
 <?php
-if ($_SERVER['SERVER_NAME'] == 'www.idsfadt.com') 
+if ($_SERVER['SERVER_NAME'] == 'www.idsfadt.com')
 {
 	$DEV = false;
+
+	//Cabal
 	$mysql_server = "mysql191.secureserver.net";
 	$mysql_user = "SRstats";
 	$mysql_password = "c4b4Lity";
-	$mysql_db = "sr";
-	
-	$CabalLoginWebUrl = "http://www.idsfadt.com/Cabal/cabal_login_page.php";
+	$mysql_db = "SRstats";
+
+	$LoginWebUrl = "http://www.idsfadt.com/Cabal/login_page.php";
 	$ScoutWebUrl = "http://www.idsfadt.com/Cabal/Scout/scout.php";
 	$DossierWebUrl = "http://www.idsfadt.com/Cabal/Dossier/Dossier.php";
+
+	//Murc
+	/*
+	$mysql_server = "mysql191.secureserver.net";
+	$mysql_user = "SRstats";
+	$mysql_password = "c4b4Lity";
+	$mysql_db = "MurcDB";
+
+	$LoginWebUrl = "http://www.idsfadt.com/Murc/login_page.php";
+	$ScoutWebUrl = "http://www.idsfadt.com/Murc/Scout/scout.php";
+	$DossierWebUrl = "http://www.idsfadt.com/Murc/Dossier/Dossier.php";
+	*/
+
+	//Shriner
+	/*
+	$mysql_server = "mysql191.secureserver.net";
+	$mysql_user = "SRstats";
+	$mysql_password = "c4b4Lity";
+	$mysql_db = "ShrinerDB";
+
+	$LoginWebUrl = "http://www.idsfadt.com/Shriner/login_page.php";
+	$ScoutWebUrl = "http://www.idsfadt.com/Shriner/Scout/scout.php";
+	$DossierWebUrl = "http://www.idsfadt.com/Shriner/Dossier/Dossier.php";
+	*/
 }
-else 
+else
 {
 	$DEV = true;
 	$mysql_server = "localhost";
 	$mysql_user = "root";
 	$mysql_password = "R0it";
 	$mysql_db = "sr";
-	
-	$CabalLoginDebugUrl = "http://localhost/Cabal/cabal_login_page.php";
+
+	$LoginDebugUrl = "http://localhost/Cabal/login_page.php";
 	$ScoutDebugUrl = "http://localhost/Cabal/Scout/scout.php";
 	$DossierDebugUrl = "http://localhost/Cabal/Dossier/Dossier.php";
 }
