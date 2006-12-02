f2fb4f361096c9d6b9fd4778eaaaa3a5a65a26be
nagten <68544113+nagten@users.noreply.github.com>
2006-12-02 10:46:48 +0000

    New planetnames

--- DEV/PHP/Cabal/DB/populatetblsrstatstable.php
+++ DEV/PHP/Cabal/DB/populatetblsrstatstable.php
@@ -18,15 +18,15 @@ body {
 //Set global variables to connect to MySQL DB
 include("../connect_to_database.php");
 
-$users = array( 0=>array('username'=>'********', 'srplanetname'=>'Infinity Edge'),
-                1=>array('username'=>'********', 'srplanetname'=>'Oven'),
-                2=>array('username'=>'********', 'srplanetname'=>'Chaos'),
-                3=>array('username'=>'******', 'srplanetname'=>'100-Aker-Forest'),
-                4=>array('username'=>'******', 'srplanetname'=>'Kindom of Loathing'),
-                5=>array('username'=>'*********', 'srplanetname'=>'Biotica'),
-                6=>array('username'=>'**********', 'srplanetname'=>'Noviy Magadan II'),
-                7=>array('username'=>'*******', 'srplanetname'=>'Thermocouple'),
-                8=>array('username'=>'RageChild', 'srplanetname'=>'The Hotel')
+$users = array( 0=>array('username'=>'********', 'srplanetname'=>'Illyria'),
+                1=>array('username'=>'********', 'srplanetname'=>'Underworld'),
+                2=>array('username'=>'********', 'srplanetname'=>'Peanut Butter'),
+                3=>array('username'=>'******', 'srplanetname'=>'Neutral Planet'),
+                4=>array('username'=>'******', 'srplanetname'=>'poetingspaintry'),
+                5=>array('username'=>'*********', 'srplanetname'=>'da Pumpatorium'),
+                6=>array('username'=>'**********', 'srplanetname'=>'Tomorrow'),
+                7=>array('username'=>'*******', 'srplanetname'=>'Juuousei'),
+                8=>array('username'=>'*********', 'srplanetname'=>'La 2019')
                 );
 
 foreach($users as $arrayuser)
@@ -45,16 +45,16 @@ foreach($users as $arrayuser)
                     fdurability, fspeed, fsensors, fstealth,
                     fmaintenance, fplanet, fdate)
                     VALUES
-                    ('". $arrayuser['srplanetname']. "', '". $arrayuser['username'] ."', ".rand(0,2000).", ".rand(0,100).", ".rand(0,2000).", ".rand(0,100).",
-                    ".rand(0,2000).", ".rand(0,100).", ".rand(0,2000).", ".rand(0,100).",
-                    ".rand(0,20000).", ".rand(0,1000000).", ".rand(-5000,5000).", ".rand(0,100).",
-                    ".rand(0,5).", ".rand(0,500000).", ".rand(-5000,5000).", ".rand(0,100).",
-                    ".rand(0,2500000).", ".rand(-250,1500).", ".rand(0,100).", ".rand(1250000,2500000).",
-                    ".rand(0,25).", ".rand(1,25).", ".rand(0,100).", ".rand(0,100). ",
-                    ".rand(0,100).", ".rand(0,100).", ".rand(0,100).", ".rand(0,100).",
-                    ".rand(0,100).", ".rand(0,100).", ".rand(0,100).", ".rand(0,100).",
-                    ".rand(0,100).", ".rand(0,100).", ".rand(0,100).", ".rand(0,100).",
-                    ".rand(0,5000). ", " .rand(0,100). ", ''
+                    ('". $arrayuser['srplanetname']. "', '". $arrayuser['username'] ."', "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", "0", "0",
+                    "0", "0", ''
                     )";
 
     $result = @mysql_query($strSqlString);
