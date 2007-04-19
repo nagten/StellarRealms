7c069e3aa1896d04ab95b55581e360da3e07b2c9
nagten <68544113+nagten@users.noreply.github.com>
2007-04-19 19:45:25 +0000

    Added more qozsh ships + orbital and surface space

--- DEV/PHP/Cabal/Scout/convariables.php
+++ DEV/PHP/Cabal/Scout/convariables.php
@@ -1,71 +1,107 @@
 <?php
-	define("conADVGE", "60"); //Advanced Genetics Lab
-	define("conADVIN", "2"); //Advanced Interceptor
-	define("conADVTS", "60"); //Advanced Technologies Shipyard
-	define("conAEGMS", "100"); //Aegis Mobile Shield
+	//Surface Structures surface space
+	define("conssADVTS", "60"); //Advanced Technologies Shipyard	
+	define("conssAIRB1", "50"); //Airbase
+	define("conssAIRB2", "75"); //Airbase Improved	
+	define("conssBARR1", "50"); //Barracks
+	define("conssBARR2", "7"); //Barracks Veteran	
+	define("conssBIOLO", "60"); //Biological Research Facility	
+	define("conssBROCE", "400"); //Brood Center	
+	define("conssDEFTU", "60"); //Defense Turret
+	define("conssDIPCO", "60"); //Diplomatic Council
+	define("conssEMBAS", "50"); //Embassy	
+	define("conssFARM1", "50"); //Farm I
+	define("conssFARM2", "50"); //Farm II
+	define("conssFARM3", "50"); //Farm III	
+	define("conssFUEL1", "50"); //Refinery (Fuel)
+	define("conssFUEL2", "60"); //Refinery (Fuel - Improved)
+	define("conssGELAB", "50"); //Genetics Lab
+	define("conssGNDHI", "140"); //Grand Hive
+	define("conssHABI1", "100"); //Habitat
+	define("conssHABI2", "120"); //Habitat (Improved)
+	define("conssHABI3", "140"); //Habitat (Ultradense)
+	define("conssHIBCA", "200"); //Hibernation Caves
+	define("conssHOSPI", "100"); //Hospital
+	define("conssINSHT", "50"); //Institute of Higher Thought
+	define("conssINTEL", "100"); //Intelligence Agency
+	define("conssINTMP", "60"); //Interplanetary Marketplace
+	define("conssINTFO", "40"); //Interstellar Forum
+	define("conssMANU1", "50"); //Manufacturing Plant
+	define("conssMANU2", "60"); //Manufacturing Plant (Improved)
+	define("conssMATS1", "50"); //Materials Processing Plant
+	define("conssMATS2", "70"); //Materials Processing Plant (Improved)
+	define("conssMATRC", "70"); //Materials Research Complex
+	define("conssMINE1", "50"); //Mining Facility (Metals)
+	define("conssMINE2", "60"); //Mining Facility (Metals - Improved)
+	define("conssRADI1", "50"); //Mining Facility (Radioactives)
+	define("conssRADI2", "60"); //Mining Facility (Radioactives - Improved)
+	define("conssRLAB1", "50"); //Research Lab
+	define("conssRLAB2", "60"); //Research Lab (Improved)
+	define("conssVINEM", "12"); //Vinemind
+	define("conssWARFA", "70"); //War Factory
+	define("conssWHSE1", "50"); //Warehouse (Small)
+	define("conssWHSE2", "70"); //Warehouse (Medium)
+	define("conssWHSE3", "90"); //Warehouse (Large)
+	define("conssSTOCK", "60"); //Stock Exchange
+	define("conssSDEF1", "50"); //Surface Defense Battery
+	define("conssSDEF2", "60"); //Surface Defense Battery (Improved)
+	define("conssSSLD1", "200"); //Surface Shield Generator
+	define("conssSSLD2", "250"); //Surface Shield Generator (Improved)
+	define("conssTANDB", "70"); //Tangler Defense Barge
+	define("conssTSCHL", "50"); //Trade School
+	define("conssUNIVE", "50"); //University
+	define("conssWEATL", "50"); //Weapons Technology Laboratory
+	
+	//Orbital Structures orbital space
+	define("conosFOLDR", "40"); //Space Folder
+	define("conosJUMP1", "50"); //Jumpgate
+	define("conosJUMP2", "60"); //Jumpgate (Improved)
+	define("conosLISTN", "30"); //Listening Post
+	define("conosOBULK", "130"); //Orbital Bulwark
+	define("conosOCON1", "50"); //Orbital Construction Yard
+	define("conosOCON2", "60"); //Orbital Construction Yard (Improved)
+	define("conosODEFM", "80"); //Orbital Defense Monitor
+	define("conosODEF1", "50"); //Orbital Defense Platform
+	define("conosODEF2", "70"); //Orbital Defense Platform (Improved)
+	define("conosOMIN1", "20"); //Orbital Minefield
+	define("conosOMIN2", "25"); //Orbital Minefield (Improved)
+	define("conosOSLD1", "100"); //Orbital Shield
+	define("conosOSLD2", "125"); //Orbital Shield (Improved)
+	define("conosPBANK", "50"); //Planetary Bank
+	define("conosPLATE", "70"); //Plating Factory
+	define("conosRSENS", "40"); //Remote Sensor Array
+	define("conosSATE1", "22"); //Satellites
+	define("conosSATE2", "30"); //Satellites (Improved)
+	define("conosSBASE", "400"); //Starbase	
+	define("conosTRACK", "60"); //Tracking Station	
+	
+	//Surface Structures
+	define("conADVTS", "60"); //Advanced Technologies Shipyard	
 	define("conAIRB1", "50"); //Airbase
-	define("conAIRB2", "75"); //Airbase Improved
-	define("conAMIPS", "1000"); //planetary shield
-	define("conANVBS", "50"); //Anvil Battleship
-	define("conASPHC", "30"); //Asp Heavy Cruiser
-	define("conAVASC", "30"); //Avalanche Siege Cruiser
-	define("conBADLC", "22"); //Badger Light Cruiser
+	define("conAIRB2", "75"); //Airbase Improved	
 	define("conBARR1", "50"); //Barracks
-	define("conBARR2", "7"); //Barracks Veteran
-	define("conBARAF", "12"); //Barracuda Attack Frigate
-	define("conBATSH", "50"); //Battleship
-	define("conBERDE", "20"); //Berzerker Destroyer
-	define("conBIOLO", "60"); //Biological Research Facility
-	define("conBLABM", "160"); //Black Widow Brood Minder
-	define("conBROCE", "400"); //Brood Center
-	define("conCOLFR", "14"); //Collector Frigate
-	define("conCOLOS", "200"); //Colossus Megaship
-	define("conCRUBC", "30"); //Crusader Battlecruiser
-	define("conCRUIS", "30"); //Cruiser
-	define("conDAGHF", "4"); //Dagger Heavy Fighter
-	define("conDEERS", "8"); //Deep Recon Scout
+	define("conBARR2", "7"); //Barracks Veteran	
+	define("conBIOLO", "60"); //Biological Research Facility	
+	define("conBROCE", "400"); //Brood Center	
 	define("conDEFTU", "60"); //Defense Turret
-	define("conDESTR", "18"); //Destroyer
 	define("conDIPCO", "60"); //Diplomatic Council
-	define("conDRAMA", "120"); //Dragon Mobile Assault Platform
-	define("conDREAD", "55"); //Dreadnought
-	define("conEMBAS", "50"); //Embassy
-	define("conFANFB", "1"); //Fang Fighter Bomber
+	define("conEMBAS", "50"); //Embassy	
 	define("conFARM1", "50"); //Farm I
 	define("conFARM2", "50"); //Farm II
-	define("conFARM3", "50"); //Farm III
-	define("conFIGBO", "2"); //Fighter Bomber
-	define("conFIGIN", "2"); //Fighter Interceptor
-	define("conFIRSD", "20"); //Fire Support Destroyer
-	define("conFOLDR", "40"); //Space Folder
-	define("conFRIGA", "12"); //Frigate
+	define("conFARM3", "50"); //Farm III	
 	define("conFUEL1", "50"); //Refinery (Fuel)
 	define("conFUEL2", "60"); //Refinery (Fuel - Improved)
 	define("conGELAB", "50"); //Genetics Lab
-	define("conGOLBA", "60"); //Goliath Battleship
 	define("conGNDHI", "140"); //Grand Hive
 	define("conHABI1", "100"); //Habitat
 	define("conHABI2", "120"); //Habitat (Improved)
 	define("conHABI3", "140"); //Habitat (Ultradense)
-	define("conHAMGU", "16"); //Hammer Gunship
-	define("conHVYBO", "2"); //Heavy Bomber
-	define("conHVYCA", "40"); //Heavy Carrier
-	define("conHVYCR", "35"); //Heavy Cruiser
 	define("conHIBCA", "200"); //Hibernation Caves
 	define("conHOSPI", "100"); //Hospital
-	define("conHURFC", "25"); //Hurricane Fast Cruiser
-	define("conIMPFR", "14"); //Improved Frigate
 	define("conINSHT", "50"); //Institute of Higher Thought
 	define("conINTEL", "100"); //Intelligence Agency
-	define("conINTFR", "12"); //Interdictor Frigate
 	define("conINTMP", "60"); //Interplanetary Marketplace
 	define("conINTFO", "40"); //Interstellar Forum
-	define("conJUDDR", "50"); //Judicator Dreadnought
-	define("conJUMP1", "50"); //Jumpgate
-	define("conJUMP2", "60"); //Jumpgate (Improved)
-	define("conLEOSC", "30"); //Leopard Strike Cruiser
-	define("conLIGCA", "30"); //Light Carrier
-	define("conLISTN", "30"); //Listening Post
 	define("conMANU1", "50"); //Manufacturing Plant
 	define("conMANU2", "60"); //Manufacturing Plant (Improved)
 	define("conMATS1", "50"); //Materials Processing Plant
@@ -75,6 +111,27 @@
 	define("conMINE2", "60"); //Mining Facility (Metals - Improved)
 	define("conRADI1", "50"); //Mining Facility (Radioactives)
 	define("conRADI2", "60"); //Mining Facility (Radioactives - Improved)
+	define("conRLAB1", "50"); //Research Lab
+	define("conRLAB2", "60"); //Research Lab (Improved)
+	define("conVINEM", "12"); //Vinemind
+	define("conWARFA", "70"); //War Factory
+	define("conWHSE1", "50"); //Warehouse (Small)
+	define("conWHSE2", "70"); //Warehouse (Medium)
+	define("conWHSE3", "90"); //Warehouse (Large)
+	define("conSTOCK", "60"); //Stock Exchange
+	define("conSDEF1", "50"); //Surface Defense Battery
+	define("conSDEF2", "60"); //Surface Defense Battery (Improved)
+	define("conSSLD1", "200"); //Surface Shield Generator
+	define("conSSLD2", "250"); //Surface Shield Generator (Improved)
+	define("conTANDB", "70"); //Tangler Defense Barge
+	define("conTSCHL", "50"); //Trade School
+	define("conUNIVE", "50"); //University
+	
+	//Orbital structures
+	define("conFOLDR", "40"); //Space Folder
+	define("conJUMP1", "50"); //Jumpgate
+	define("conJUMP2", "60"); //Jumpgate (Improved)
+	define("conLISTN", "30"); //Listening Post
 	define("conOBULK", "130"); //Orbital Bulwark
 	define("conOCON1", "50"); //Orbital Construction Yard
 	define("conOCON2", "60"); //Orbital Construction Yard (Improved)
@@ -85,46 +142,72 @@
 	define("conOMIN2", "25"); //Orbital Minefield (Improved)
 	define("conOSLD1", "100"); //Orbital Shield
 	define("conOSLD2", "125"); //Orbital Shield (Improved)
-	define("conORCBA", "50"); //Orca Battleship
 	define("conPBANK", "50"); //Planetary Bank
 	define("conPLATE", "70"); //Plating Factory
-	define("conPRIHC", "30"); //Privateer Heavy Cruiser
-	define("conRAVMC", "30"); //Raven Missile Cruiser
 	define("conRSENS", "40"); //Remote Sensor Array
-	define("conRLAB1", "50"); //Research Lab
-	define("conRLAB2", "60"); //Research Lab (Improved)
 	define("conSATE1", "22"); //Satellites
 	define("conSATE2", "30"); //Satellites (Improved)
-	define("conSCOUT", "7"); //Scout
-	define("conSBASE", "400"); //Starbase
-	define("conSTIDR", "2"); //Stinger Drone
-	define("conSTOCK", "60"); //Stock Exchange
-	define("conSDEF1", "50"); //Surface Defense Battery
-	define("conSDEF2", "60"); //Surface Defense Battery (Improved)
-	define("conSSLD1", "200"); //Surface Shield Generator
-	define("conSSLD2", "250"); //Surface Shield Generator (Improved)
-	define("conTANDB", "70"); //Tangler Defense Barge
+	define("conSBASE", "400"); //Starbase	
+	define("conTRACK", "60"); //Tracking Station
+	define("conWEATL", "50"); //Weapons Technology Laboratory
+	
+	//Ships
+	define("conADVGE", "60"); //Advanced Genetics Lab
+	define("conADVIN", "2"); //Advanced Interceptor	
+	define("conAEGMS", "100"); //Aegis Mobile Shield	
+	define("conAMIPS", "1000"); //planetary shield
+	define("conANVBS", "50"); //Anvil Battleship
+	define("conASPHC", "30"); //Asp Heavy Cruiser
+	define("conAVASC", "30"); //Avalanche Siege Cruiser
+	define("conBADLC", "22"); //Badger Light Cruiser	
+	define("conBARAF", "12"); //Barracuda Attack Frigate
+	define("conBATSH", "50"); //Battleship
+	define("conBBFIL", "1"); //bundle of black filaments
+	define("conBERDE", "20"); //Berzerker Destroyer
+	define("conBLABM", "160"); //Black Widow Brood Minder	
+	define("conCOLFR", "14"); //Collector Frigate
+	define("conCOLOS", "200"); //Colossus Megaship
+	define("conCRUBC", "30"); //Crusader Battlecruiser
+	define("conCRUIS", "30"); //Cruiser
+	define("conDAGHF", "4"); //Dagger Heavy Fighter
+	define("conDEERS", "8"); //Deep Recon Scout	
+	define("conDESTR", "18"); //Destroyer	
+	define("conDRAMA", "120"); //Dragon Mobile Assault Platform
+	define("conDREAD", "55"); //Dreadnought
+	define("conFANFB", "1"); //Fang Fighter Bomber
+	define("conFIGBO", "2"); //Fighter Bomber
+	define("conFIGIN", "2"); //Fighter Interceptor
+	define("conFIRSD", "20"); //Fire Support Destroyer	
+	define("conFRIGA", "12"); //Frigate	
+	define("conGOLBA", "60"); //Goliath Battleship	
+	define("conHAMGU", "16"); //Hammer Gunship
+	define("conHVYBO", "2"); //Heavy Bomber
+	define("conHVYCA", "40"); //Heavy Carrier
+	define("conHVYCR", "35"); //Heavy Cruiser	
+	define("conHURFC", "25"); //Hurricane Fast Cruiser
+	define("conIMPFR", "14"); //Improved Frigate	
+	define("conINTFR", "12"); //Interdictor Frigate	
+	define("conJUDDR", "50"); //Judicator Dreadnought	
+	define("conLEOSC", "30"); //Leopard Strike Cruiser
+	define("conLIGCA", "30"); //Light Carrier		
+	define("conORCBA", "50"); //Orca Battleship	
+	define("conPRIHC", "30"); //Privateer Heavy Cruiser
+	define("conRAVMC", "30"); //Raven Missile Cruiser	
+	define("conSCOUT", "7"); //Scout	
+	define("conSTIDR", "2"); //Stinger Drone	
 	define("conTARA1", "60"); //Taratula Attack Station Mark I
 	define("conTARA2", "60"); //Taratula Attack Station Mark II
+	define("conTARA3", "75"); //Taratula Attack Station Mark III
+	define("conTARA4", "60"); //Taratula Attack Station Mark III
 	define("conTARA5", "60"); //Taratula Attack Station Mark V
 	define("conTERCA", "50"); //Terrapin Carrier
 	define("conTORBA", "60"); //Tortoise Battleship
-	define("conTRACK", "60"); //Tracking Station
-	define("conTSCHL", "50"); //Trade School
-	define("conUNIVE", "50"); //University
 	define("conVENHF", "5"); //Venom Heavy Fighter
-	define("conVESSC", "50"); //Vespa Siege Carrier
-	define("conVINEM", "12"); //Vinemind
-	define("conWARFA", "70"); //War Factory
-	define("conWHSE1", "50"); //Warehouse (Small)
-	define("conWHSE2", "70"); //Warehouse (Medium)
-	define("conWHSE3", "90"); //Warehouse (Large)
+	define("conVESSC", "50"); //Vespa Siege Carrier	
 	define("conWASFI", "3"); //Wasp Fighter
-	define("conWAYEC", "32"); //Wayfarer Exploration Cruiser
-	define("conWEATL", "50"); //Weapons Technology Laboratory
+	define("conWAYEC", "32"); //Wayfarer Exploration Cruiser	
 	define("conZEPFD", "18"); //Zephyr Fast Destroyer
-	define("conBBFIL", "1"); //bundle of black filaments
-
+	
 	//Airbase capacity
 	define("conAIRB1CAP", "200");
 	define("conAIRB2CAP", "300");
